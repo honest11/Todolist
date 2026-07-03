685cecf48360bfeaa48b4a41263c6b24d01fc4d2
Insanity <honestbright11@gmail.com>
2026-07-03 05:50:17 -0700

    css style file

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+// select all needed elements
+
+const input = document.getElementById('todo-input');
+const btnAdd = document.getElementById('add-button');
+const ulTodolist = document.getElementById('todo-list')
+
+
