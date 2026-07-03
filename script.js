3bf70268ca2b07bfd1488eff8c388c97f5906cd0
Insanity <honestbright11@gmail.com>
2026-07-03 06:11:44 -0700

    add input clear after hitting add btn

--- script.js
+++ script.js
@@ -13,15 +13,14 @@ btnAdd.addEventListener('click', (e)=>{
 
     // an event to stop auto refresh in website
     e.preventDefault()
-
     // Creating a <i></li> element
     let li = document.createElement('li')
     // appending the input values typed into the <i></li> element which was created
     li.append(input.value)
-
     // appending the <i></li> element to my document body
     document.body.appendChild(li)
-
     // appending the <i></li> element to my ul tag
     ulTodolist.appendChild(li)
+    // empties the input fields after hitting the add button
+    input.value = ''
 })
